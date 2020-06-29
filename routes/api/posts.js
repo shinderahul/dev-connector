e29c8cec75acb53af0bb6dde12e5d157536bfc67
Rahul <rahul.shinde@digitas.com>
2020-06-29 19:58:30 +0530

    Get all posts and single post by id

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -6,6 +6,7 @@ const { check, validationResult } = require('express-validator');
 const Posts = require('../../models/Posts');
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
+const { json } = require('express');
 
 // @route   POST api/posts
 // @desc    Create a Post
@@ -37,5 +38,26 @@ router.post('/', [auth, [
     }
 
 });
+
+// @route   GET api/posts/:id
+// @desc    Get post by ID
+// @access  Private
+router.get('/:id', auth ,async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id).sort({ date: -1 });
+        
+        if(!post) {
+            return res.status(4040).json({ msg: 'Post not found' })
+        }
+        
+        res.json(post);
+    } catch (err) {
+        console.error(err.message);
+        if(err.kind === 'ObjectId') {
+            return res.status(4040).json({ msg: 'Post not found' })
+        }
+        res.status(500).send('Server Error')
+    }
+})
 
 module.exports = router;
