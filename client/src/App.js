b24ae3349d6b4eb19553bc5a0c96562e397be037
Rahul <rahul.shinde@digitas.com>
2020-07-01 18:04:09 +0530

    Initial components

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import React, { Fragment } from 'react';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
 import './App.css';
 
 const App = () => (
-  <Fragment>
-    <h1>App</h1>
-  </Fragment>
-)
+	<Fragment>
+		<Navbar />
+		<Landing />
+	</Fragment>
+);
 
 export default App;
