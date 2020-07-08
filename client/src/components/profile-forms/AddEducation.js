cc650a6a1c3b0d7ab0296742c3c8ac8e356a58e9
Rahul <rahul.shinde@digitas.com>
2020-07-08 12:38:22 +0530

    Deleted education, experience and account

--- client/src/components/profile-forms/AddEducation.js
+++ client/src/components/profile-forms/AddEducation.js
@@ -91,7 +91,7 @@ const AddEducation = ({ addEducation, history }) => {
 								setFormData({ ...formData, current: !current });
 							}}
 						/>{' '}
-						Current Job
+						Current School
 					</p>
 				</div>
 				<div className='form-group'>
