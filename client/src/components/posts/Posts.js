909194fc452007c068738b8ae31376361f93a1e0
Rahul <rahul.shinde@digitas.com>
2020-07-09 19:26:12 +0530

    Post item Component

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -2,6 +2,7 @@ import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
+import PostItem from './PostItem';
 import { getPost } from '../../actions/post';
 
 const Posts = ({ getPost, post: { posts, loading } }) => {
@@ -9,7 +10,22 @@ const Posts = ({ getPost, post: { posts, loading } }) => {
 		getPost();
 	}, [getPost]);
 
-	return <div></div>;
+	return loading ? (
+		<Spinner />
+	) : (
+		<Fragment>
+			<h1 className='large text-primary'>Posts</h1>
+			<p className='lead'>
+				<i className='fas fa-user'></i> Welcome to the community!
+			</p>
+			{/* PostForm */}
+			<div className='posts'>
+				{posts.map((post) => (
+					<PostItem key={post._id} post={post} />
+				))}
+			</div>
+		</Fragment>
+	);
 };
 
 Posts.propTypes = {
