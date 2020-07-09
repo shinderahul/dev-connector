4d5a9a98d670a0992bd425670571112d92afead1
Rahul <rahul.shinde@digitas.com>
2020-07-09 20:18:29 +0530

    Delete Post

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -3,13 +3,14 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Moment from 'react-moment';
 import { connect } from 'react-redux';
-import { addLike, removeLike } from '../../actions/post';
+import { addLike, removeLike, deletePost } from '../../actions/post';
 
 const PostItem = ({
 	auth,
 	post: { _id, text, name, avatar, user, likes, comments, date },
 	addLike,
 	removeLike,
+	deletePost,
 }) => {
 	return (
 		<div className='post bg-white p-1 my-1'>
@@ -45,7 +46,11 @@ const PostItem = ({
 					)}
 				</Link>
 				{!auth.loading && user === auth.user._id && (
-					<button type='button' className='btn btn-danger'>
+					<button
+						onClick={(e) => deletePost(_id)}
+						type='button'
+						className='btn btn-danger'
+					>
 						<i className='fas fa-times'></i>
 					</button>
 				)}
@@ -59,10 +64,13 @@ PostItem.propTypes = {
 	post: PropTypes.object.isRequired,
 	addLike: PropTypes.func.isRequired,
 	removeLike: PropTypes.func.isRequired,
+	deletePost: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = (state) => ({
 	auth: state.auth,
 });
 
-export default connect(mapStateToProps, { addLike, removeLike })(PostItem);
+export default connect(mapStateToProps, { addLike, removeLike, deletePost })(
+	PostItem
+);
